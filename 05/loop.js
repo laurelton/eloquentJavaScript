a22c3cfee82746beaf340086ee07cb5bd88bbc83
Laurelton <name@example.com>
2022-10-07 12:31:12 -0400

    your own loop

---
+++ 05/loop.js
@@ -0,0 +1,34 @@
+/*
+ * Write a higher-order function loop that provides something like a for loop
+ * statement. It takes a value, a test function, an update function, and a body
+ * function. Each iteration, it first runs the test function on the current loop
+ * value and stops if that returns false. Then it calls the body function,
+ * giving it the current value. Finally, it calls the update function to create
+ * a new value and starts from the beginning.
+ *
+ * When defining the function, you can use a regular loop to do the actual
+ * looping.
+ */
+
+/**
+ * @description Function to mimic a 'for' loop.
+ * @param value The initial value for the loop
+ * @param test Function run on value to determine when to exit the function
+ * @param update Function used to update the value
+ * @param body Function used to mimic the body of a 'for' loop
+ */
+function loop(value, test, update, body) {
+    while (test(value)) {
+        body(value);
+        value = update(value);
+    }
+}
+
+const main = () => {
+    loop(3, n => n > 0, n => n - 1, console.log);
+    // → 3
+    // → 2
+    // → 1
+};
+
+main();
